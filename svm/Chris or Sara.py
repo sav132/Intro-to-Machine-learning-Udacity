2f66542485bc33580d0b0e8b7e911ef2aefad92f
sav132 <saviorajan1998@gmail.com>
2017-09-01 16:11:08 +0530

    second mini project

---
+++ svm/Chris or Sara.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
+
+    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+
+
+#########################################################
+### your code goes here ###
+
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
+
+clf = SVC(kernel = 'rbf',C =10000)
+clf.fit(features_train,labels_train)
+t0 = time()
+
+pred1 = clf.predict(features_test[10])
+pred2 = clf.predict(features_test[26])
+pred3 = clf.predict(features_test[50])
+
+print "predicting time:", round(time()-t0, 3), "s"
+
+print pred1,pred2,pred3
+
+
+
